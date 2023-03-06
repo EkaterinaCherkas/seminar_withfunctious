0e87f55f515625a9841e9c68554f156831e2ad82
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-06 20:01:12 +0300

    add task about принимает на вход число и показывает кол-во цифр, n/=10

--- task1/Program.cs
+++ task1/Program.cs
@@ -6,7 +6,7 @@
 int GetSum(int number){
 int sum = 0 ;
 for (int i = 1; i<=number; i++){
-    sum += i ;}
+    sum += i ;}  // sum = sum + i;
     return sum;
 }
 Console.Clear();
---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает 
+//на вход число N и выдаёт количество цифр в числе.
+
+Console.Clear();
+Console.WriteLine("Введите число ");
+int N = int.Parse(Console.ReadLine());
+
+int Count(int number){ //123
+    int colvo = 0;
+    if (number < 0){
+        number = number * (-1); //n*=(-1)
+    }
+    while (number > 0){
+        number = number / 10; // n/=10
+        colvo++; // colvo =3
+    }
+    return colvo;
+}
+Console.WriteLine($"Количество цифр = {Count(N)}");
