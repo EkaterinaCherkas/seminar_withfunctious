8c9784c83d95388888ffceed7c6bc3ca553e7119
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-06 19:02:47 +0300

    add task1 with teacher

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//24 Напишите программу, которая принимает на вход число A
+// и выдаёт сумму чисел от 1 до A
+// a = 5
+// sum = 1 + 2 + 3 + 4 + 5
+
+int GetSum(int number){
+int sum = 0 ;
+for (int i = 1; i<=number; i++){
+    sum += i ;}
+    return sum;
+}
+Console.Clear();
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int result = GetSum(number);
+Console.WriteLine($"Сумма от 1 до {number} = {result}");
